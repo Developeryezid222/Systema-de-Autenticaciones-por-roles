6a47bc858ff2608d5d10577d5cf1b43e136544ba
Developeryezid222 <codigojava23@gmail.com>
2026-03-04 19:50:18 -0500

    manejo de errores

--- Sistema-Autenticacion-Autorizacion-FrontEnd/src/app/app.config.ts
+++ Sistema-Autenticacion-Autorizacion-FrontEnd/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations'; // Cambio aquí
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { provideHttpClient, withInterceptors  } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
